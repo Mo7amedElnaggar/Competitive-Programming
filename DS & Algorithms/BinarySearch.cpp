3b27434fc43089cd7d56b1c86c20489ee194c8a4
Mo7amedElnaggar <NaggarQ@gmail.com>
2017-02-25 07:08:00 +0200

    update BinarySearch Technique

--- DS & Algorithms/BinarySearch.cpp
+++ DS & Algorithms/BinarySearch.cpp
@@ -6,23 +6,31 @@ using namespace std ;
 typedef long long ll ;
 vector <int> vec ;
 
+// l , r --> the Range that you wanna search in 
+// k --> is the key that you wanna search for in Range [l , r]
+// List must be sorted.
 ll BinarySearch(int l , int r , int k){ // Recursive
-	if(l > r) return -1 ;
-	int mid = (l + r) / 2;
-	if(vec[mid] > k)
+	if(l > r) return -1 ;  // k is not in the list :(
+	int mid = (l + r) / 2; // get Mid point 
+	if(vec[mid] > k) // if true  , then you should go to the left branch
 		return BinarySearch(l , mid - 1 , k) ;
-	else if(vec[mid] < k)
-		return BinarySearch(mid + 1 , r , k) ;
+	else if(vec[mid] < k) // if true , then you should go to the right branch
+		return BinarySearch(mid + 1 , r , k) ; 
 
-	return mid ;
+	return mid ;  // if you come to here , ok you must found 'K' in the list
 }
 
-ll BinarySearch2(int l , int r , int k){
-	while(l < r){
-		int mid = (l + r) / 2 ;
-		if(vec[mid] > k) r = mid -1 ;
-		else l = mid + 1 ;
+// l , r --> the Range that you wanna search for
+// k --> is the key that you wanna search for in Range [l , r]\
+// List must be sorted don't forget :D
+ll BinarySearch2(int l , int r , int k){ // iterative 
+	while(l < r){ // if not , then k is not existd in the list
+		int mid = (l + r) / 2 ;  // get mid point
+		if(vec[mid] > k) r = mid -1 ; // go to left
+		else l = mid + 1 ; //  go to the right
 	}
-	if(l == r && vec[l] == k) return l ;
-	return -1 ;
+	// this 'if' statment check again if i found k or not
+	if(l == r && vec[l] == k) return l ;  
+	return -1 ; // for sure you didnt found it :(
 }
+
