528ae5c52465112e2f058fb8198a6203c0d5b967
Mo7amedElnaggar <NaggarQ@gmail.com>
2017-03-08 23:35:50 +0200

    solved Day 22

---
+++ HackerRank/30 Days of Code/Day22_BinarySearchTrees.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+
+using namespace std ;
+typedef long long ll ;
+
+class Node{
+public:
+    int data;
+    Node *left;
+    Node *right;
+    Node(int d){
+        data = d;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+class Solution {
+public :
+    Node* insert(Node* root, int data) {
+        if(root == NULL)
+            return new Node(data) ;
+        else {
+            Node * cur ;
+            if(data <= root -> data){
+                cur = insert(root -> left , data) ;
+                root -> left = cur ;
+            }
+            else {
+                cur = insert(root->right, data);
+                root->right = cur;
+            }
+        }
+        return root ;
+    }
+    int getHeight(Node* root){
+        if(root == NULL) return -1 ;
+
+        return max(getHeight(root -> left) , getHeight(root -> right)) + 1 ;
+    }
+};
+
+int main(){
+#ifndef ONLINE_JUDGE
+    //freopen("input.txt" , "rt" , stdin) ;
+    //freopen("output.txt" , "wt" , stdout) ;
+#endif
+    Solution myTree;
+    Node* root = NULL;
+    int t;
+    int data;
+
+    cin >> t;
+
+    while(t-- > 0){
+        cin >> data;
+        root = myTree.insert(root, data);
+    }
+    int height = myTree.getHeight(root);
+    cout << height;
+
+
+
+    return 0 ;
+}
