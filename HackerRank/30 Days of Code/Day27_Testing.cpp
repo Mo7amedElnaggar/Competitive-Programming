cb4b3142534fb8b83e446f566c784b05d8a7e354
Mo7amedElnaggar <NaggarQ@gmail.com>
2017-04-08 10:18:31 +0200

    solved Day27 generate Testcase

---
+++ HackerRank/30 Days of Code/Day27_Testing.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll ;
+
+int main(){
+   printf("%d\n" , 5);
+
+   printf("%d %d\n" , 5 , 3) ;
+   printf("%d %d %d %d %d\n" ,1 , 0 , -1 , 4 , 5);
+
+   printf("%d %d\n" , 4 , 2 );
+   printf("%d %d %d %d\n" ,1, 0, -1, 1);
+
+   printf("%d %d\n" ,7 , 4);
+   printf("%d %d %d %d %d %d %d\n" ,1 , 0, 1, -1, -1, -2, 3);
+
+   printf("%d %d\n" ,3, 3);
+   printf("%d %d %d\n" ,-1 , 0 , 1);
+
+   printf("%d %d\n" ,5 , 5);
+   printf("%d %d %d %d %d\n" ,-1 , 0, 0, 3, 1);
+
+    return 0;
+}
+
+
+//YES
+//NO
+//NO
+//YES
+//YES
+
+
